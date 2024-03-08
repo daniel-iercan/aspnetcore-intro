652dd8bec364e3d48253522fd2f7f7345d1a61f3
Alexandru Rus <Alexandru.Rus@theaccessgroup.com>
2024-03-08 14:12:40 +0200

    initial commit of razor pages movie project

--- src/RazorPagesMovie/Pages/Index.cshtml.cs
+++ src/RazorPagesMovie/Pages/Index.cshtml.cs
@@ -1,7 +1,7 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
-namespace Timetable.Pages
+namespace RazorPagesMovie.Pages
 {
     public class IndexModel : PageModel
     {
